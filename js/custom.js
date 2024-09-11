4ddee92c5d089fc83b449e6239cb1617abe119ca
nilesh7272 <nileshparadva97@gmail.com>
2024-09-11 19:34:04 +0530

    bhagvat gita api upload

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ const getDeta = () => {
                         <p class="fw-bold fs-5 border-bottom pb-3"> Hindi translation :- ${data.tej.ht}</p>
                         <p class="fw-bold fs-5  pb-3"> Transliteration :- ${data.transliteration}</p>` 
             }).catch(error => {
-                console.error('Error:', error);
+                `<h2 class="text-danger">Data not found ${error}</h2>`
             });
     } else {
         console.log("get data error");
